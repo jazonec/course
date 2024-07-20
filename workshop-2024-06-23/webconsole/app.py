200426806cb8f653b50a0a882654116725827b68
jazonec <sldubinsky@yandex.ru>
2024-07-20 15:59:54 +0300

    Исправил внешний ключ

--- workshop-2024-06-23/webconsole/app.py
+++ workshop-2024-06-23/webconsole/app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, session, redirect, url_for, request
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Session
-from sqlalchemy import DateTime, Integer, String, Numeric, ForeignKey, select, update, create_engine, BIGINT
+from sqlalchemy import DateTime, Boolean, String, Numeric, ForeignKey, select, update, create_engine, BIGINT, func, sql
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 import logging
 
-pg_host = os.getenv("DB_HOST")
+pg_host = os.getenv("DB_HOST") + ':5532'
 pg_user = os.getenv("POSTGRES_USER")
 pg_pass = os.getenv("POSTGRES_PASSWORD")
 pg_base = os.getenv("POSTGRES_DB")
@@ -19,7 +19,9 @@ class Base(DeclarativeBase):
 app = Flask(__name__)
 app.secret_key = 'some_strong_secret_key'
 
-engine = create_engine("postgresql://${pg_user}:${pg_pass}@${pg_host}/${pg_base}", echo=True)
+connection_string = f"postgresql://{pg_user}:{pg_pass}@{pg_host}/{pg_base}"
+print(connection_string)
+engine = create_engine(connection_string, echo=True)
 
 class User(Base):
     __tablename__ = "users"
@@ -27,17 +29,17 @@ class User(Base):
     user_id: Mapped[BIGINT] = mapped_column(BIGINT, primary_key=True)
     username: Mapped[str] = mapped_column(String(120))
     email: Mapped[str]
-    created = mapped_column(DateTime, nullable=False)
-    is_admin: Mapped[bool]
-    allow_prompt: Mapped[bool]
-    allow_dalle: Mapped[bool]
+    created = mapped_column(DateTime, nullable=False, server_default=func.now())
+    is_admin: Mapped[bool] = mapped_column(Boolean, server_default=sql.false())
+    allow_prompt: Mapped[bool] = mapped_column(Boolean, server_default=sql.false())
+    allow_dalle: Mapped[bool] = mapped_column(Boolean, server_default=sql.false())
 
     user_balance: Mapped["UserBalance"] = relationship(back_populates="user")
 
 class UserBalance(Base):
     __tablename__ = "user_balance"
 
-    user_id = mapped_column(BIGINT, ForeignKey("user.user_id"), primary_key=True)
+    user_id = mapped_column(BIGINT, ForeignKey("users.user_id"), primary_key=True)
     balance = mapped_column(Numeric(15,2))
     
     user: Mapped["User"] = relationship(back_populates="user_balance")
@@ -87,8 +89,10 @@ def logout():
 @app.route('/balance')
 def balance():
     if ('username' in session):
-        userdata = session_engine.scalars(select(UserBalance).where(UserBalance.user_id == session['user_id'])).first()
-        userbalance = '0' if userdata == None else str(userdata.balance)
+        userbalance = '0'
+        if session['user_id'] != None:
+            userdata = session_engine.scalars(select(UserBalance).where(UserBalance.user_id == session['user_id'])).first()
+            userbalance = '0' if userdata == None else str(userdata.balance)
         return render_template('balance.html', userbalance=userbalance)
     return redirect(url_for('login'))
 
