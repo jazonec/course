533c79b01bcf23e0d616c28569263208cc059327
jazonec <sldubinsky@yandex.ru>
2024-08-01 22:26:30 +0300

    Настроил проверку веб-админки

--- workshop/webconsole/database.py
+++ workshop/webconsole/database.py
@@ -1,6 +1,9 @@
+'''Работа с базой данных'''
 import os
+from dataclasses import dataclass
 
-from sqlalchemy import DateTime, Boolean, String, Numeric, ForeignKey, select, update, create_engine, BIGINT, func, sql
+from sqlalchemy import DateTime, Boolean, String, Numeric, BIGINT
+from sqlalchemy import  ForeignKey, select, update, create_engine, func, sql
 from sqlalchemy.orm import DeclarativeBase, Session
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -10,15 +13,20 @@ pg_user = os.getenv("POSTGRES_USER")
 pg_pass = os.getenv("POSTGRES_PASSWORD")
 pg_base = os.getenv("POSTGRES_DB")
 
-engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_base}", echo=True)
+engine = create_engine(
+    f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_base}",
+    echo=True)
 
+@dataclass
 class Base(DeclarativeBase):
   pass
 
 def get_engine():
     return Session(engine)
 
+@dataclass
 class User(Base):
+    '''Пользователи бота'''
     __tablename__ = "users"
 
     user_id: Mapped[BIGINT] = mapped_column(BIGINT, primary_key=True)
@@ -31,7 +39,9 @@ class User(Base):
 
     user_balance: Mapped["UserBalance"] = relationship(back_populates="user")
 
+@dataclass
 class UserBalance(Base):
+    '''Баланс пользователей бота'''
     __tablename__ = "user_balance"
 
     user_id = mapped_column(BIGINT, ForeignKey("users.user_id"), primary_key=True)
@@ -40,6 +50,7 @@ class UserBalance(Base):
     user: Mapped["User"] = relationship(back_populates="user_balance")
 
 # class UserBalanceDetail(Base):
+#    '''Запросы пользователей бота'''
 #    __tablename__ = "user_balance_detail"
 #
 #    id: Mapped[int] = mapped_column(Integer, primary_key=True)
